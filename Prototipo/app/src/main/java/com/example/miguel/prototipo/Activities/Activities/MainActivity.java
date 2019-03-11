5e0843672f0fbda87c782d98ef765fbfa23aafda
MiguelBalderrama97 <37118731+MiguelBalderrama97@users.noreply.github.com>
2019-03-11 12:39:39 -0700

    Update MainActivity.java

--- Prototipo/app/src/main/java/com/example/miguel/prototipo/Activities/Activities/MainActivity.java
+++ Prototipo/app/src/main/java/com/example/miguel/prototipo/Activities/Activities/MainActivity.java
@@ -45,7 +45,7 @@ public class MainActivity extends AppCompatActivity  implements NavigationView.O
     private ImageView imgNull;
 
     private boolean vacio = true;
-    //Comentario chido
+    //Comentario chido obvis
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
