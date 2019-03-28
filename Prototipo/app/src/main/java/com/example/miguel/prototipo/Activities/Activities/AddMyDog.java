c692bd646b302653aef1d6fd9a0f980c35e242bf
MiguelBalderrama97 <37118731+MiguelBalderrama97@users.noreply.github.com>
2019-03-27 22:26:27 -0700

    Cargar fotos INCOMPLETO
    Falta guardar las fotos en variables

--- Prototipo/app/src/main/java/com/example/miguel/prototipo/Activities/Activities/AddMyDog.java
+++ Prototipo/app/src/main/java/com/example/miguel/prototipo/Activities/Activities/AddMyDog.java
@@ -1,7 +1,9 @@
 package com.example.miguel.prototipo.Activities.Activities;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
+import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -10,6 +12,7 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -23,9 +26,17 @@ public class AddMyDog extends AppCompatActivity {
 
     private Spinner spinnerRaza;
     private ImageButton btnImg1, btnImg2, btnImg3, btnImg4;
+    private ImageView icon1, icon2, icon3, icon4;
 
     private FirebaseDatabase database = FirebaseDatabase.getInstance();
     private DatabaseReference reference = database.getReference(MainActivity.PATH_DOGS);
+
+    private Intent inCamera = new Intent("android.media.action.IMAGE_CAPTURE");
+
+    private final int PICTURE_FROM_CAMERA = 50;
+    private final int PICTURE_FROM_CAMERA2 = 100;
+    private final int PICTURE_FROM_CAMERA3 = 150;
+    private final int PICTURE_FROM_CAMERA4 = 200;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,12 +48,42 @@ public class AddMyDog extends AppCompatActivity {
         btnImg3 = findViewById(R.id.btnImg3);
         btnImg4 = findViewById(R.id.btnImg4);
 
+        icon1 = findViewById(R.id.icon1);
+        icon2 = findViewById(R.id.icon2);
+        icon3 = findViewById(R.id.icon3);
+        icon4 = findViewById(R.id.icon4);
+
         spinnerRaza = findViewById(R.id.spinnerRaza);
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.razas_array, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinnerRaza.setAdapter(adapter);
 
         getSupportActionBar().setTitle("Agregar a mis perros...");
+
+        btnImg1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivityForResult(inCamera, PICTURE_FROM_CAMERA);
+            }
+        });
+        btnImg2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivityForResult(inCamera, PICTURE_FROM_CAMERA2);
+            }
+        });
+        btnImg3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivityForResult(inCamera, PICTURE_FROM_CAMERA3);
+            }
+        });
+        btnImg4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivityForResult(inCamera, PICTURE_FROM_CAMERA4);
+            }
+        });
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab2);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -59,4 +100,47 @@ public class AddMyDog extends AppCompatActivity {
         });
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        switch (requestCode) {
+            case PICTURE_FROM_CAMERA:
+                if (resultCode == Activity.RESULT_OK) {
+                    String result = data.toUri(0);
+                    Toast.makeText(this, "Result: " + result, Toast.LENGTH_SHORT).show();
+                    icon1.setImageResource(R.mipmap.ic_checked);
+                } else {
+                    Toast.makeText(this, "No funciono!!", Toast.LENGTH_SHORT).show();
+                }
+                break;
+            case PICTURE_FROM_CAMERA2:
+                if (resultCode == Activity.RESULT_OK) {
+                    String result = data.toUri(0);
+                    Toast.makeText(this, "Result: " + result, Toast.LENGTH_SHORT).show();
+                    icon2.setImageResource(R.mipmap.ic_checked);
+                } else {
+                    Toast.makeText(this, "No funciono!!", Toast.LENGTH_SHORT).show();
+                }
+                break;
+            case PICTURE_FROM_CAMERA3:
+                if (resultCode == Activity.RESULT_OK) {
+                    String result = data.toUri(0);
+                    Toast.makeText(this, "Result: " + result, Toast.LENGTH_SHORT).show();
+                    icon3.setImageResource(R.mipmap.ic_checked);
+                } else {
+                    Toast.makeText(this, "No funciono!!", Toast.LENGTH_SHORT).show();
+                }
+                break;
+            case PICTURE_FROM_CAMERA4:
+                if (resultCode == Activity.RESULT_OK) {
+                    String result = data.toUri(0);
+                    Toast.makeText(this, "Result: " + result, Toast.LENGTH_SHORT).show();
+                    icon4.setImageResource(R.mipmap.ic_checked);
+                } else {
+                    Toast.makeText(this, "No funciono!!", Toast.LENGTH_SHORT).show();
+                }
+                break;
+            default:
+                super.onActivityResult(requestCode, resultCode, data);
+        }
+    }
 }
